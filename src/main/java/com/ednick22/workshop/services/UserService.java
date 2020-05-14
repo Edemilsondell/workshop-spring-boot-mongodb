3821d6d198e2503d244d08207fee75d05b4a226b
Edemilson da Mata <edemilsondell@hotmail.com>
2020-05-13 22:51:53 -0400

    Obtendo um usuário por id

--- src/main/java/com/ednick22/workshop/services/UserService.java
+++ src/main/java/com/ednick22/workshop/services/UserService.java
@@ -1,21 +1,29 @@
 package com.ednick22.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ednick22.workshop.domain.User;
 import com.ednick22.workshop.repository.UserRepository;
+import com.ednick22.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-		
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 
 }
