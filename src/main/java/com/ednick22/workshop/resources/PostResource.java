879a8847dec2c384af20ea4b2bd500f785456274
Edemilson da Mata <edemilsondell@hotmail.com>
2020-05-16 21:56:40 -0400

    Consulta simples com query methods

--- src/main/java/com/ednick22/workshop/resources/PostResource.java
+++ src/main/java/com/ednick22/workshop/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.ednick22.workshop.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ednick22.workshop.domain.Post;
+import com.ednick22.workshop.resources.util.URL;
 import com.ednick22.workshop.services.PostService;
 
 @RestController
@@ -23,6 +26,13 @@ public class PostResource {
 	public Object findById(@PathVariable String id) {
 		Optional<Post> obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public Object findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);		
+		return ResponseEntity.ok().body(list);
 	}
 
 }
--- src/main/java/com/ednick22/workshop/services/PostService.java
+++ src/main/java/com/ednick22/workshop/services/PostService.java
@@ -1,5 +1,6 @@
 package com.ednick22.workshop.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,10 @@ public class PostService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
